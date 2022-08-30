6de6a4a0b71533c7a1b5dd948bb7b2ee7781513c
David Abadie <abadiedaviiid@gmail.com>
2022-08-30 20:18:21 -0300

    frase.equals eureka

---
+++ src/ejercicios_aprendizaje/Ejercicio7.java
@@ -0,0 +1,22 @@
+/*
+Crear un programa que pida una frase y si esa frase es igual a “eureka” el programa
+pondrá un mensaje de Correcto, sino mostrará un mensaje de Incorrecto. Nota: investigar
+la función equals() en Java.
+ */
+package ejercicios_aprendizaje;
+
+import java.util.Scanner;
+
+
+public class Ejercicio7 {
+    public static void main(String[] args){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese la frase:");
+        String frase = leer.nextLine();
+        if (frase.equals("eureka")) {
+            System.out.println("CORRECTO :)");
+        } else {
+            System.out.println("INCORRECTO :(");
+        }
+    }
+}
