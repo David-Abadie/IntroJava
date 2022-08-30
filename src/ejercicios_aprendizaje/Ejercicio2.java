2a75977d03bb03ed90eb9b27a08e1e9be62bd42f
David Abadie <abadiedaviiid@gmail.com>
2022-08-30 19:28:09 -0300

    pedir nombre y mostrarlo con variable

---
+++ src/ejercicios_aprendizaje/Ejercicio2.java
@@ -0,0 +1,15 @@
+/*
+Escribir un programa que pida tu nombre, lo guarde en una variable y lo muestre por
+pantalla.
+ */
+package ejercicios_aprendizaje;
+import java.util.Scanner;
+
+public class Ejercicio2 {
+    public static void main(String[] args){
+        Scanner leer = new Scanner(System.in);
+        System.out.println("Ingrese su nombre:");
+        String nombre = leer.nextLine();
+        System.out.println("Su nombre es " + nombre);
+    }
+}
