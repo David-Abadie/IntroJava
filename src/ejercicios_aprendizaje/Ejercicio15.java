87154700afea0915b9ee669a372e0d27e6a3eb25
David Abadie <abadiedaviiid@gmail.com>
2022-09-01 23:47:15 -0300

    vector decreciente primeros 100 numeros

---
+++ src/ejercicios_aprendizaje/Ejercicio15.java
@@ -0,0 +1,16 @@
+/*
+Realizar un algoritmo que rellene un vector con los 100 primeros números enteros y los
+muestre por pantalla en orden descendente.
+ */
+package ejercicios_aprendizaje;
+
+public class Ejercicio15 {
+    public static void main(String[] args){
+        int [] vector = new int[100];
+        for (int i = 99; i >= 0 ; i--) {
+            vector[i]= i ;
+            System.out.print("["+ vector[i] +"]");
+        }
+        System.out.println("");
+    }
+}
